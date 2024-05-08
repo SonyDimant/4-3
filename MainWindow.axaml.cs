4cb7d633c6f8c1988aa77d9c79949da00543397c
Sophizm <dimantsofa@gmail.com>
2024-05-08 21:43:08 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using System;
+
+namespace NewA;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    //public void Button11_Click(object sender, RoutedEventArgs e)
+    //{
+    //    if (Random.Next(0,2)>0)
+    //    {
+    //        Button11.Background = Brushes.Red;
+    //    }
+    //    else
+    //    {
+    //        Button11.Background = Brushes.Blue;
+    //    }
+    //}
+}
